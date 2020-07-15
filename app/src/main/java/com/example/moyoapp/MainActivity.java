0d49e8d9798f5bf567d481af9c24a0746ace68a6
qendev <qendev00@gmail.com>
2020-07-16 02:37:44 +0300

    created the menu drawer for the different menu items in the moyo mobile app

--- app/src/main/java/com/example/moyoapp/MainActivity.java
+++ app/src/main/java/com/example/moyoapp/MainActivity.java
@@ -1,39 +1,105 @@
 package com.example.moyoapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    private static int SPLASH_SCREEN_TIME_OUT=2000;
+import fragments.BpReadingsFragments;
+import fragments.DataFragment;
+import fragments.DoctorFragment;
+import fragments.SettingsFragment;
+import fragments.StatisticsFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout MdrawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i=new Intent(MainActivity.this,
-                        FirstScreenActivity.class);
-                //Intent is used to switch from one activity to another.
+        //set the toolbar as the actionbar
+        Toolbar mtoolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mtoolbar);
 
-                startActivity(i);
-                //invoke the SecondActivity.
+        MdrawerLayout = findViewById(R.id.navigationDrawer_layout);
 
-                finish();
-                //the current activity will get finished.
-            }
-        }, SPLASH_SCREEN_TIME_OUT);
+        //get a refference to the navigation view to be able to handle the click events on the menu drawer
+        NavigationView mnavigationView = findViewById(R.id.nav_view);
+
+        //set the listener
+        mnavigationView.setNavigationItemSelectedListener(this);
+
+
+        //for rotating the hamburger icon together with the drawer.
+        ActionBarDrawerToggle mtoggle = new ActionBarDrawerToggle(this, MdrawerLayout, mtoolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        MdrawerLayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                    new BpReadingsFragments()).commit();
+            mnavigationView.setCheckedItem(R.id.nav_bp_redeadings);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_bp_redeadings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new BpReadingsFragments()).commit();
+                break;
+
+            case R.id.nav_statistics:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new StatisticsFragment()).commit();
+                break;
+
+            case R.id.nav_data:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new DataFragment()).commit();
+                break;
+
+            case R.id.nav_doctor:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new DoctorFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_privacy_policy:
+                Toast.makeText(this, "Privacy Policy Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_help_feedback:
+                Toast.makeText(this, "Help and Feedback clicked!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        MdrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (MdrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            MdrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+
+
+        }
     }
 }
