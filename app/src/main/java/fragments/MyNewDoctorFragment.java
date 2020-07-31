368b8b3c021a1983e0ab1d0e3565e6be064b8c45
qendev <qendev00@gmail.com>
2020-07-31 18:43:03 +0300

    started creating the my new doctor UI layout

---
+++ app/src/main/java/fragments/MyNewDoctorFragment.java
@@ -0,0 +1,23 @@
+package fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.moyoapp.R;
+
+
+public class MyNewDoctorFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_new_doctor, container, false);
+    }
+}
