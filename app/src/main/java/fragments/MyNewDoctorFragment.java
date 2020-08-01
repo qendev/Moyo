893b7931e4272c8585063238b03fcb311b3b93ee
qendev <qendev00@gmail.com>
2020-08-01 13:19:33 +0300

    finished creating my doctor layout and call and message capabilities in the layout

--- app/src/main/java/fragments/MyNewDoctorFragment.java
+++ app/src/main/java/fragments/MyNewDoctorFragment.java
@@ -1,5 +1,7 @@
 package fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +9,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.moyoapp.R;
 
 
 public class MyNewDoctorFragment extends Fragment {
 
+    private ImageView img_call_mydoctor,img_message_mydoctor;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_new_doctor, container, false);
+      view= inflater.inflate(R.layout.fragment_my_new_doctor, container, false);
+
+        img_call_mydoctor =view.findViewById(R.id.img_call_mydoctor);
+        img_message_mydoctor = view.findViewById(R.id.img_message_mydoctor);
+
+        img_message_mydoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:"));
+                startActivity(sendIntent);
+            }
+        });
+
+        img_call_mydoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.intent.action.DIAL");
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
