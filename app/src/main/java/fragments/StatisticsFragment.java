ab29fd9716a48c2ff07df0062c689606a9df2ae0
qendev <qendev00@gmail.com>
2020-08-27 10:39:46 +0300

    latest commit

--- app/src/main/java/fragments/StatisticsFragment.java
+++ app/src/main/java/fragments/StatisticsFragment.java
@@ -1,36 +1,34 @@
 package fragments;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.moyoapp.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import models.response.PatientData;
-import models.response.ResponseBpReadings;
-import models.response.ResponsePatientDataNew;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import services.MoyoService;
-import services.MyConfig;
-import services.RetrofitRequest;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 
 public class StatisticsFragment extends Fragment {
 
-    private Button buttonPatientData;
+    LineChartView lineChartView;
 
+    String[] axisData = {"Mon","Tue","Wed","Thur","Fri","Sat","Sun" };
+    int[] yAxisData = {50, 60, 70, 80, 90, 100, 110, 120};
 
 
 
@@ -42,50 +40,57 @@ public class StatisticsFragment extends Fragment {
         View view;
         view = inflater.inflate(R.layout.fragment_statistics, container, false);
 
-        buttonPatientData=view.findViewById(R.id.buttonPatientData);
+        lineChartView = view.findViewById(R.id.bpreadingsline);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+        Line line = new Line(yAxisValues);
+        line = new Line(yAxisValues).setColor(Color.parseColor("#7147e8"));
+
+        line = new Line(yAxisValues);
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
 
 
+        lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+         data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setName("Days");
+        axis.setTextSize(20);
+        axis.setTextColor(Color.parseColor("#000000"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Blood Pressure Readings");
+        yAxis.setTextColor(Color.parseColor("#000000"));
+        yAxis.setTextSize(20);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
 
 
-
-
-
-        buttonPatientData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SharedPreferences sharedPref = getContext().getSharedPreferences(MyConfig.SHARED_PREF_USER, Context.MODE_PRIVATE);
-                String token = sharedPref.getString("token", "");
-                String patient_id= sharedPref.getString("id", "");
-
-
-
-                MoyoService moyoService = RetrofitRequest.getRetrofitInstance().create(MoyoService.class);
-                Call<ArrayList<ResponseBpReadings>> arrayListCall=moyoService.getBpReadings("Baerer "+token,patient_id);
-                arrayListCall.enqueue(new Callback<ArrayList<ResponseBpReadings>>() {
-                    @Override
-                    public void onResponse(Call<ArrayList<ResponseBpReadings>> call, Response<ArrayList<ResponseBpReadings>> response) {
-                        Log.e("GraphTest!",String.valueOf(response.body()));
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<ArrayList<ResponseBpReadings>> call, Throwable t) {
-
-                    }
-                });
-
-
-
-
-
-
-
-
-
-
-            }
-        });
 
 
 
